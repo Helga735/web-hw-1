a1b2208078ded0a6cde3718cf61f3901ea7c8380
Olha <142577158+Helga735@users.noreply.github.com>
2024-01-07 21:35:20 +0200

    new

--- flask-docker-app/app.py
+++ flask-docker-app/app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return "Hello World!"
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+  app.run(debug=False, host='0.0.0.0')
